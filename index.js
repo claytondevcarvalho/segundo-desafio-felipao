cf51d943f918afdaf6ff4eb81cf59d56eee3d8eb
Clayton Dev Carvalho <claytondevcarvalho@gmail.com>
2023-11-17 05:43:35 -0300

    primeira versão do código no arquivo index.js

---
+++ index.js
@@ -0,0 +1,33 @@
+let saldo = diferencaVitoriasDerrotas(50,60)
+let nomeRank = ["Ferro","Bronze","Prata","Ouro","Diamante","Lendário","Imortal"]
+let numeroRank = 0
+
+calcularRank()
+console.log(`O Herói tem um saldo de ${saldo} vitórias e está no nível ${nomeRank[numeroRank]}!!!`)
+
+function diferencaVitoriasDerrotas(vitorias = 0 , derrotas = 0){
+    return vitorias - derrotas;
+}
+function calcularRank(){
+    if (saldo<10){
+        numeroRank = 0
+    }
+    else if (saldo >= 10 && saldo <=20){
+        numeroRank = 1
+    }
+    else if (saldo > 20 && saldo <=50){
+        numeroRank = 2
+    }
+    else if (saldo > 50 && saldo <=80){
+        numeroRank = 3
+    }
+    else if (saldo > 80 && saldo <=90){
+        numeroRank = 4
+    }
+    else if (saldo > 90 && saldo <=100){
+        numeroRank = 5
+    }else {
+        numeroRank = 6
+    }
+    return numeroRank
+}
